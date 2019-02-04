0e2f0f6a34e35558a1db3bea9359fe2178936e8a
wenbinke <wenbinke@gmail.com>
2019-02-04 12:06:49 +0800

    f

--- src/lib/db/index.js
+++ src/lib/db/index.js
@@ -2,16 +2,9 @@ import Dexie from 'dexie'
 
 const db = new Dexie('codepad')
 db.version(1).stores({
-  code_items: '++id,name,language,code,created_at,updated_at,deleted_at',
-  life_items: '++id,name,key,created_at,updated_at,deleted_at'
+  code_items: '++id,title,language,code,created_at,updated_at,deleted_at'
 })
 
-db.on('ready', async () => {
-  // await db.life_items.add({
-  //   id: 1,
-  //   name: 'Unicode Text Convertor',
-  //   key: 'unicode_text_convertor'
-  // })
-})
+db.on('ready', async () => {})
 
 export default db
